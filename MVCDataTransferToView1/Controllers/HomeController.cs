8e166575e66be5dcb01ee2de5a435b310a6a1b57
UĞUR BAĞCI <ugur_bagci_@hotmail.com>
2019-06-21 15:32:49 +0300

    Add project files.

---
+++ MVCDataTransferToView1/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDataTransferToView1.Controllers
+{
+    public class HomeController : Controller
+    {
+       //09.04.2019
+
+
+
+        public ActionResult Index()
+        {
+            ViewBag.Baslik = "MVC ile veri gönderimi";
+            ViewBag.AdSoyad = "Can canan";
+            ViewBag.Sayi1 = 5;
+            ViewBag.Sayi2 = 10;
+            // açıklama var
+            List<string> isimler = new List<string>();
+            isimler.Add("CediOsman");
+            isimler.Add("Mayk");
+            isimler.Add("Izzet");
+            isimler.Add("Yasemin");
+            isimler.Add("Aslı");
+            isimler.Add("Layla");
+            // açıklama var
+
+            ViewBag.isimler = isimler;
+            // açıklama var
+            List<string> soyisimler = new List<string>();
+            soyisimler.Add("Osmanoğlu");
+            soyisimler.Add("Muratoğlu");
+            soyisimler.Add("Ahmetoğlu");
+            soyisimler.Add("Cansuoğlu");
+            soyisimler.Add("Leylaoğlu");
+
+            ViewData["soyisimler"] = soyisimler;
+
+
+            TempData["AdSoyad"] = "Can Oğuz";
+            return View();
+        }
+
+
+    }
+}
